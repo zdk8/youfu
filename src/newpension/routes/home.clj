5d401c0c5030e92e7e41ba3a25b92f24f1d2ee4a
tq <405256493@qq.com>
2014-08-18 09:37:08 +0800

    commit

--- src/newpension/routes/home.clj
+++ src/newpension/routes/home.clj
@@ -1,15 +1,41 @@
 (ns newpension.routes.home
   (:require [compojure.core :refer :all]
             [newpension.layout :as layout]
-            [newpension.util :as util]))
+            [newpension.util :as util]
+            [newpension.controller.old :as old]))
 
 (defn home-page []
   (layout/render
     "home.html" {:content (util/md->html "/md/docs.md")}))
 
-(defn about-page []
-  (layout/render "about.html"))
+(defn update-page []
+  (layout/render "update.html"))
+
+(defn addold-page []
+  (layout/render "addold.html"))
+
+(defn old-page []
+  (layout/render "old.html"))
+
+(defn login-page []
+  (layout/render "login.html"))
 
 (defroutes home-routes
-  (GET "/" [] (home-page))
-  (GET "/about" [] (about-page)))
+  (GET "/" [] (login-page))
+  ;    (home-page))
+  (POST "/login" [username password] (old/login username password))
+  (GET "/update" [] (update-page))
+  (GET "/addold" [] (addold-page))
+  (POST "/saveold" request (old/create-old request))
+  (GET "/old" [] (old/get-olds))
+  (POST "/old" [name] (old/get-olds name))
+  (GET "/search" [id] (home/get-old id))
+  ;  (GET "/searchid" [id] (home/get-oldbyid id))
+  (GET "/log" [functionid] (old/get-logs functionid))
+  (POST "/updateold" request (old/update-old request))
+  (POST "/deleteold" request (old/delete-old request))
+  (GET "/audit" [functionid loginname dvcode] (old/get-audits functionid loginname dvcode))
+  (POST "/checkaudit" [flag aulevel digest tprkey auditid dvcode loginname username opseno] (old/update-audit flag aulevel digest tprkey auditid dvcode loginname username opseno))
+  (GET "/log" [functionid] (old/get-logs functionid))
+  (GET "/func" [username] (old/get-funcs username))
+  )
