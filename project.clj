4ca0c18b3a4116520dd33b7f87ae88c2e675cfa0
tq <405256493@qq.com>
2014-08-18 09:10:34 +0800

    commit

--- project.clj
+++ project.clj
@@ -10,10 +10,12 @@
                  [markdown-clj "0.9.47"]
                  [environ "0.5.0"]
                  [im.chit/cronj "1.0.1"]
-                 [noir-exception "0.2.2"]]
+                 [noir-exception "0.2.2"]
+                 [com.oracle/ojdbc6 "11.2.0.3"]
+                 [korma "0.3.1"]]
 
   :repl-options {:init-ns newpension.repl}
-  :jvm-opts ["-server"]
+  :jvm-opts ["-Dfile.encoding=utf8"]
   :plugins [[lein-ring "0.8.10"]
             [lein-environ "0.5.0"]]
   :ring {:handler newpension.handler/app
@@ -30,4 +32,24 @@
          :injections [(require 'pjstadig.humane-test-output)
                       (pjstadig.humane-test-output/activate!)]
          :env {:dev true}}}
+  :repositories [
+                  ["java.net" "http://download.java.net/maven/2"]
+                  ["nexus" "https://code.lds.org/nexus/content/groups/main-repo"]
+                  ["sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
+                               ;; If a repository contains releases only setting
+                               ;; :snapshots to false will speed up dependencies.
+                               :snapshots false
+                               ;; Disable signing releases deployed to this repo.
+                               ;; (Not recommended.)
+                               :sign-releases false
+                               ;; You can also set the policies for how to handle
+                               ;; :checksum failures to :fail, :warn, or :ignore.
+                               :checksum :fail
+                               ;; How often should this repository be checked for
+                               ;; snapshot updates? (:daily, :always, or :never)
+                               :update :always
+                               ;; You can also apply them to releases only:
+                               :releases {:checksum :fail :update :always}}]
+
+                  ]
   :min-lein-version "2.0.0")
