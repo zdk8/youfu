34600bd432b96d3330abd263ff3deba38195bdb1
Administrator <1521511388@qq.com>
2014-11-24 17:52:34 +0800

    lhd

--- resources/public/js/views/pension/YangLaoXGDlg.js
+++ resources/public/js/views/pension/YangLaoXGDlg.js
@@ -1,19 +1,40 @@
 define(function(){
     function render(local,option){
-        /*确定*/
-        local.find('[opt=determine]').click(function(){
-            local.find('[opt=yljgdlg]').form('submit',{
-                url:'aaaa',
-                success:function(data){
-                    console.log(data)
-                }
-            });
-        });
+        var ylxgdlg = local.find('[opt=ylxgdlg]');      //表单
+        var determine = local.find('[opt=determine]');      //确定按钮
+        var actiontype = option.actiontype;             //操作方式
+
+        if(actiontype == "update"){                     //编辑
+            ylxgdlg.form('load', option.data);
+            determinefunc({determine:determine,ylxgdlg:ylxgdlg,actiontype:actiontype})
+        }else if(actiontype == "add"){                  //新增
+            console.log("add")
+        }
 
         /*取消*/
         local.find('[opt=cancle]').click(function(){
             option.parent.trigger('close');
         })
+    }
+    /*确定按钮*/
+    var determinefunc = function(params){
+        params.determine.click(function(){
+            if(params.actiontype == "add"){         //新增
+                params.ylxgdlg.form('submit',{
+                    url:'aaaa',
+                    success:function(data){
+                        console.log(data)
+                    }
+                });
+            }else if(params.actiontype == "update"){     //修改
+                params.ylxgdlg.form('submit',{
+                    url:'aaaa',
+                    success:function(data){
+                        console.log(data)
+                    }
+                });
+            }
+        });
     }
 
     return {
--- resources/public/js/views/pension/YangLaoXGManagement.js
+++ resources/public/js/views/pension/YangLaoXGManagement.js
@@ -1,18 +1,102 @@
 define(function(){
     var render = function(local,option){
+        addylxgFun(local);                     //添加养老机构
+        var ylxggl = local.find('[opt=yanlaoxgmanagement]');        //养老机构管理
+        var refresh = local.find('[opt=refresh]');        //刷新
+        ylxggl.datagrid({
+            url:'queryyljg',
+            type:'post',
+            onLoadSuccess:function(data){
+                console.log(data)
+                var updates = local.find('[action=update]');           //修改
+                var del = local.find('[action=delete]');                //删除
+                var rows=data.rows;
+                var btns_arr=[updates,del];
+                for(var i=0;i<rows.length;i++){
+                    for(var j=0;j<btns_arr.length;j++){
+                        (function(index){
+                            var record=rows[index];
+                            $(btns_arr[j][i]).click(function(){
+                                /*修改*/
+                                if($(this).attr("action") == "update"){
+//                                    console.log(record.departname)
+                                    console.log(record)
+                                    var data = record;
+                                    var departname = record.departname;         //机构名称
+                                    updateylxgFun(local,departname,data)                //修改养老机构
+                                    /*cj.showContent({
+                                     title:record.biaozhunmingcheng+'修改',
+                                     htmfile:'text!views/dmxt/PlaceCommon.htm',
+                                     jsfile:'views/dmxt/PlaceCommon',
+                                     queryParams:{
+                                     id:record.id,
+                                     actionType:"update"*//*,
+                                     tablename:tablename,
+                                     wholename:wholename,
+                                     headname:record.leibiemingcheng*//*
+                                     }
+                                     })*/
+                                }
+                                /*删除*/
+                                if($(this).attr("action") == "delete"){
+//                                        console.log(record.id)
+                                    /*$.post(
+                                     '',
+                                     {
+                                     id:record.id,
+                                     tablename:wholename
+                                     },
+                                     function(data){
+                                     var data = eval('(' + data + ')');
+                                     if(data.success){
+                                     alert("删除成功")
+                                     //                                                loaddate();
+                                     }
+                                     }
+                                     )*/
+                                }
+                            });
+                        })(i)
+                    }
+                }
+            }
+        })
+        refresh.click(function(){
+            console.log("refresh")
+            ylxggl.datagrid('reload');
+        })
+
+
+    }
+
+    /*添加养老机构*/
+    var addylxgFun = function(local){
         local.find('[opt=addylxg]').click(function(){
             require(['commonfuncs/popwin/win','text!views/pension/YangLaoXGDlg.htm','views/pension/YangLaoXGDlg'],
                 function(win,htmfile,jsfile){
                     win.render({
-                        title:'添加星光老年之家机构',
+                        title:'添加养老机构',
                         width:350,
                         height:385,
                         html:htmfile,
+                        /*buttons:[
+                         {text:'取消',handler:function(html,parent){
+                         parent.trigger('close');
+                         }},
+                         {
+                         text:'保存1',
+                         handler:function(html,parent){
+                         //                                    local.find(html+'[opt=yljgdlg]')
+                         console.log(local.find(html+'[opt=yljgdlg]'))
+                         }
+                         }
+                         ],*/
                         renderHtml:function(local,submitbtn,parent){
                             jsfile.render(local,{
                                 submitbtn:submitbtn,
                                 act:'c',
                                 parent:parent,
+                                actiontype:'add',       //操作方式
                                 onCreateSuccess:function(data){
                                     parent.trigger('close');
                                 }
@@ -23,6 +107,44 @@ define(function(){
             )
         })
     }
+    /*修改养老机构*/
+    var updateylxgFun = function(local,departname,data){
+        require(['commonfuncs/popwin/win','text!views/pension/YangLaoXGDlg.htm','views/pension/YangLaoXGDlg'],
+            function(win,htmfile,jsfile){
+                win.render({
+                    title:'<label style="font-weight: bold;color: rgba(39,42,40,0.83)">编辑-'+departname+'</label>',
+                    width:350,
+                    height:385,
+                    html:htmfile,
+                    /*buttons:[
+                     {text:'取消',handler:function(html,parent){
+                     parent.trigger('close');
+                     }},
+                     {
+                     text:'保存1',
+                     handler:function(html,parent){
+                     //                                    local.find(html+'[opt=yljgdlg]')
+                     console.log(local.find(html+'[opt=yljgdlg]'))
+                     }
+                     }
+                     ],*/
+                    renderHtml:function(local,submitbtn,parent){
+                        jsfile.render(local,{
+                            submitbtn:submitbtn,
+                            act:'c',
+                            parent:parent,
+                            actiontype:'update',       //操作方式
+                            data:data,                   //填充数据
+                            onCreateSuccess:function(data){
+                                parent.trigger('close');
+                            }
+                        })
+                    }
+                })
+            }
+        )
+    }
+
 
     return {
         render:render
