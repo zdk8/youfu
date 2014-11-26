c0ccae324a4b4b77e5f45d661a0cb71a37bea57a
tongdongsheng <815052901@qq.com>
2014-11-26 09:39:16 +0800

    odp add deptypexml

--- src/newpension/controller/department.clj
+++ src/newpension/controller/department.clj
@@ -68,7 +68,8 @@
         {identityid :identityid}params
         checkop (get-oldpeople identityid)
         nowtime (common/get-nowtime)
-        opddate (select-keys params deppeople)]
+        opddate (map #(conj % {:checkintime nowtime}) (select-keys params deppeople))]
+    (println "DDDDDDD"  opddate)
     (if (> (count checkop) 0) (let[opdate (select-keys params oldpeople)]   (old/create-old request))  )
     (db/add-oldpeopledep opddate)
     (resp/json {:success true :message "add success"})))
