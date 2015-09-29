c53197716015855f3695074fdb867e217927a11f
Administrator <77212312@qq.com>
2015-09-29 11:15:49 +0800

    party 20150929 branch

--- src/partymgt/controller/controller.clj
+++ src/partymgt/controller/controller.clj
@@ -292,7 +292,8 @@
         page (:page params)
         name (:name params)
         credentialsnumb (:credentialsnumb params)
-        conds (str " and isdel is null " (common/likecond "name" name) (common/likecond "credentialsnumb" credentialsnumb))
+        isreceive (:isreceive params)
+        conds (str " and isdel is null " (common/likecond "name" name) (common/likecond "credentialsnumb" credentialsnumb) (if (> (count isreceive) 0) (if (= isreceive "1") (str " and  isreceive = " isreceive) (str " and  isreceive is null " ))))
         getresults (common/fenye rows page "t_certificate" "*" conds "")]
     (resp/json {:total (:total getresults) :rows (common/dateymd-bf-list (:rows getresults) "birthday" "validity" "handdate")})))
 
@@ -332,8 +333,8 @@
         page (:page params)
         name (:name params)
         credentialsnumb (:credentialsnumb params)
-        isreceive (:isreceive params)
-        receivecond (if (= isreceive "1") (str " and isreceive = 1 ") (str " and isreceive is null "))
+        isback (:isback params)
+        receivecond (if (> (count isback) 0) (if (= isback "0") (str " and isreceive = 1 ") (str " and isreceive is null ")))
         conds (str receivecond (common/likecond "name" name) (common/likecond "credentialsnumb" credentialsnumb))
         getsql (str "select c_id,name,gender,birthday,credentialstype,credentialsnumb,validity,handdate,manager,c_comments,isreceive,cr_id,receivedate,returndate,cr_comments from(select c.*,cr.cr_id,cr.receivedate,cr.returndate,cr.cr_comments from t_certificatereceive cr left join t_certificate c on cr.c_id = c.c_id)")
         getresults (common/fenye rows page (str "(" getsql ")") "*" conds " order by cr_id desc ")]
