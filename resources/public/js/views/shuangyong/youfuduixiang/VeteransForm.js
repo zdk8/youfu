2c6905e424eccff15379cd83ec668660903b4e18
Administrator <1521511388@qq.com>
2015-10-28 16:22:31 +0800

    lhd20151028

--- resources/public/js/views/shuangyong/youfuduixiang/VeteransForm.js
+++ resources/public/js/views/shuangyong/youfuduixiang/VeteransForm.js
@@ -2,12 +2,28 @@ define(function(){
     /*伤残人员*/
     function scryFunc(local, option){
         layer.load(2);
-        cj.getChildTab(local.find('[opt=vetabs]'),'ScryTable',option);
+        cj.getChildTab(local.find('[opt=vetabs]'),'ScryTable',option,"add");
     }
     /*三属人员*/
     function ssryFunc(local, option){
         layer.load(2);
-        cj.getChildTab(local.find('[opt=vetabs]'),'SsryTable',option);
+        cj.getChildTab(local.find('[opt=vetabs]'),'SsryTable',option,"add");
+    }
+    function lcryFunc(local, option){
+        layer.load(2);
+        cj.getChildTab(local.find('[opt=vetabs]'),'LcryTable',option,"add");
+    }
+    function zxlfryFunc(local, option){
+        layer.load(2);
+        cj.getChildTab(local.find('[opt=vetabs]'),'ZxlfryTable',option,"add");
+    }
+    function dbhxryFunc(local, option){
+        layer.load(2);
+        cj.getChildTab(local.find('[opt=vetabs]'),'DbhxryTable',option,"add");
+    }
+    function ybtyryFunc(local, option){
+        layer.load(2);
+        cj.getChildTab(local.find('[opt=vetabs]'),'YbtyryTable',option,"add");
     }
 
     var render=function(local,option){
@@ -25,6 +41,18 @@ define(function(){
                         break;
                     case 'ssry':        //三属人员
                         ssryFunc(local, option);
+                        break;
+                    case 'lcry':        //两参人员
+                        lcryFunc(local, option);
+                        break;
+                    case 'zxlfry':        //在乡老复人员
+                        zxlfryFunc(local, option);
+                        break;
+                    case 'dbhxry':        //带病回乡人员
+                        dbhxryFunc(local, option);
+                        break;
+                    case 'ybtyry':        //一般退役军人
+                        ybtyryFunc(local, option);
                         break;
                     default :
                         break;
